52df67531e8b9cb4057a232e1c525ce2c1b9a74d
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-11-01 10:17:09 +0900

    [main] move blog.js to blog.tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { PageProps, graphql } from "gatsby";
+import get from "lodash/get";
+import { Helmet } from "react-helmet";
+import styles from "./blog.module.css";
+import { Layout } from "../components/Layout";
+import { ArticlePreview } from "../components/ArticlePreview";
+
+const BlogIndex = ({ data, location }: PageProps) => {
+  const siteTitle = get(data, "site.siteMetadata.title");
+  const posts = get(data, "allContentfulBlogPost.edges");
+
+  return (
+    <Layout>
+      <div style={{ background: "#fff" }}>
+        <Helmet title={siteTitle} />
+        <div className={styles.hero}>Blog</div>
+        <div className="wrapper">
+          <h2 className="section-headline">Recent articles</h2>
+          <ul className="article-list">
+            {posts.map(({ node }: any) => {
+              return (
+                <li key={node.slug}>
+                  <ArticlePreview article={node} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogIndex;
+
+export const pageQuery = graphql`
+  query BlogIndexQuery {
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`;
