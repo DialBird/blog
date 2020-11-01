0c1c57c79a1fe0969e4a585603f49dfe7022594e
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-11-01 11:26:33 +0900

    [main] add features page

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -6,21 +6,21 @@ import get from 'lodash/get'
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import styles from './products.module.scss'
+import styles from './features.module.scss'
 
-const ProductsIndex = ({ data }: PageProps) => {
+const FeaturesIndex = ({ data }: PageProps) => {
   const siteTitle = get(data, 'site.siteMetadata.title')
-  const products = get(data, 'allContentfulBlogPost.edges')
+  const features = get(data, 'allContentfulBlogPost.edges')
 
   return (
     <Layout>
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
-        <div className={styles.hero}>Products</div>
+        <div className={styles.hero}>Features</div>
         <div className="wrapper">
-          <h2 className="section-headline">Recent products</h2>
+          <h2 className="section-headline">Recent features</h2>
           <ul className="article-list">
-            {products.map(({ node }: any) => {
+            {features.map(({ node }: any) => {
               return (
                 <li key={node.slug}>
                   <ArticlePreview article={node} />
@@ -34,10 +34,10 @@ const ProductsIndex = ({ data }: PageProps) => {
   )
 }
 
-export default ProductsIndex
+export default FeaturesIndex
 
 export const pageQuery = graphql`
-  query ProductsIndexQuery {
+  query FeaturesIndexQuery {
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
